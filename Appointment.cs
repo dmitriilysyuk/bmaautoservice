71d8096e99a73da0384ba8df697cbb67129ab536
dmitriilysyuk <152875377+dmitriilysyuk@users.noreply.github.com>
2023-12-21 19:06:34 +0300

    Add files via upload

---
+++ Appointment.cs
@@ -0,0 +1,16 @@
+﻿
+// В данной модели указываются параматры для элемента коллекции прошедших записей
+
+namespace bmaservise.Models
+{
+	public class Appointment
+	{
+        public string Id { get; set; }
+        public string Date { get; set; }
+        public string ServiceName { get; set; }
+        public string CarNumber { get; set; }
+        public string CarModel { get; set; }
+        public string Comment{ get; set; }
+	}
+}
+
---
+++ AutoService.cs
@@ -0,0 +1,12 @@
+﻿
+// В данной модели указываются параматры для элемента коллекции услуг автосервиса
+
+namespace bmaservise.Models
+{
+	public class AutoService
+	{
+        public string Value { get; set; }
+        public int Key { get; set; }
+    }
+}
+
